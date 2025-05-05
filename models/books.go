9a125b1c8163fce15819c4dc3e71c115710602c0
Muhammad Harits Syaifulloh <muhammadharits@icloud.com>
2025-05-05 09:22:05 +0700

    feat: add before create and update hook

--- models/books.go
+++ models/books.go
@@ -1,7 +1,26 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Book struct {
-	ID     uint   `json:"id" gorm:"primary_key"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
+	ID        uint      `json:"id" gorm:"primary_key"`
+	Title     string    `json:"title"`
+	Author    string    `json:"author"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+func (book *Book) BeforeCreate(db *gorm.DB) error {
+	book.CreatedAt = time.Now().Local()
+	book.UpdatedAt = time.Now().Local()
+	return nil
+}
+
+func (book *Book) BeforeUpdate(db *gorm.DB) error {
+	book.UpdatedAt = time.Now().Local()
+	return nil
 }
