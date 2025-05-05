bdfa6e85785ca11935c5897a9d7f16082285cb23
Muhammad Harits Syaifulloh <muhammadharits@icloud.com>
2025-05-05 09:20:48 +0700

    feat: move db connection to config

---
+++ config/database.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"github.com/askmhs/gin-book-store/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	err = database.AutoMigrate(
+		&models.Book{},
+	)
+
+	if err != nil {
+		return
+	}
+
+	DB = database
+}
